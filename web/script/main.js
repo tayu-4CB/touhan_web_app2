7e4226cc6c0014c00071dbc1f177e4300bd03d01
tayu <takenoyuichi1227@outlook.jp>
2023-09-06 21:34:06 +0900

    edit main page

--- web/script/main.js
+++ web/script/main.js
@@ -1,5 +1,6 @@
 console.log("holle1")
 document.getElementById("user_panel").style.visibility = hidden;
+
 function show_login_form(){
     let element = document.getElementById("user_panel");
     document.querySelector("#user_panel").classList.toggle("show");
@@ -13,4 +14,32 @@ function show_login_form(){
 
 function show_menu(){
     console.log("showmenue")
+}
+
+
+function tabSwitch(){
+
+    let active_tab_element = document.getElementsByClassName('is_active');
+    console.log(active_tab_element[0]);
+    document.getElementsByClassName('is_active')[0].classList.remove("is_active");
+    event.target.classList.add("is_active");
+    console.log(event.target)
+
+    document.getElementsByClassName("is_show")[0].classList.remove("is_show");
+    const array_tabs = Array.prototype.slice.call(document.getElementsByClassName('tab'));
+    const index = array_tabs.indexOf(event.target);
+    document.getElementsByClassName("panel")[index].classList.add("is_show");
+
+    console.log("holle");
+}
+
+function check_all(bool){
+    let element = document.querySelectorAll(".is_show > div > label > .selecter");
+    const num = element.length;
+    //console.log(element);
+    //console.log(num);
+    for(i=0; i<num; i++){
+        console.log(element[i].checked)
+        element[i].checked = bool;
+    }
 }
